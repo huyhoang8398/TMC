facf0b39c160266c232daeecae0057f1363d75b5
kn <huyhoang8398@gmail.com>
2022-02-21 02:21:44 +0100

    rebase

--- my-app/build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20210221-203859";
-const char *build_timestamp = "2021-02-21T20:38:59Z";
-const char *build_version = "1.0";
--- my-app/build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20210221-203859/2.19.1-gd52032f";
-const char *mg_build_timestamp = "2021-02-21T20:38:59Z";
-const char *mg_build_version = "2.19.1";
